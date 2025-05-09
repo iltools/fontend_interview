c6928526899f6f814c89e97123cdec58887fbd3f
iltools <15387673@qq.com>
2025-05-09 16:16:00 +0800

    feat: 调整目录结构，配置文件

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,7 +2,7 @@
  * @Author: iltools 15387673@qq.com
  * @Date: 2025-05-09 13:16:10
  * @LastEditors: iltools 15387673@qq.com
- * @LastEditTime: 2025-05-09 13:58:13
+ * @LastEditTime: 2025-05-09 16:11:31
  * @FilePath: \frontend_interview\.dumirc.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -15,7 +15,12 @@ export default defineConfig({
   },
   outputPath: 'docs-dist',
   themeConfig: {
-    name: 'iltools-frontend-interview',
+    // logo右边title名称
+    name: '面试宝典',
+    // 设置logo
+    // logo: '',
+    // 显示字数
+    showLineNum: true
   },
   presets: [require.resolve('@dumijs/preset-vue')],
 });
--- docs/guide.md
+++ docs/guide.md
@@ -1,1 +1,7 @@
+---
+nav: 介绍
+title: begin
+---
+
+# 开始
 This is a guide example.
