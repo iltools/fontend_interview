8e84fa38d5b97112537bec08753ca9ceecc69255
iltools <15387673@qq.com>
2025-05-09 22:46:38 +0800

    feat: embed嵌入其他md

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,17 +1,13 @@
-/*
- * @Author: iltools 15387673@qq.com
- * @Date: 2025-05-09 13:16:10
- * @LastEditors: iltools 15387673@qq.com
- * @LastEditTime: 2025-05-09 16:11:31
- * @FilePath: \frontend_interview\.dumirc.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import { defineConfig } from 'dumi';
+import path from 'path';
 
 export default defineConfig({
   apiParser: {},
   resolve: {
     entryFile: 'src/index.ts',
+  },
+  alias: {
+    '@articles': path.resolve(__dirname, 'articles'),
   },
   outputPath: 'docs-dist',
   themeConfig: {
@@ -20,7 +16,7 @@ export default defineConfig({
     // 设置logo
     // logo: '',
     // 显示字数
-    showLineNum: true
+    showLineNum: true,
   },
   presets: [require.resolve('@dumijs/preset-vue')],
 });
---
+++ docs/practice/JavaScript.md
@@ -0,0 +1,8 @@
+---
+nav: 场景题
+order: 1
+---
+
+## 前端如何实现截图?
+
+<embed src="@articles/前端如何实现截图.md"></embed>
